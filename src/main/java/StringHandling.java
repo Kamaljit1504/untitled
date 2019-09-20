a4aac67da648e2082e721f465704b71c04758e63
Kamaljit <kaurkamaljit963@gmail.com>
2019-09-20 18:41:47 -0400

    string functions

--- src/main/java/StringHandling.java
+++ src/main/java/StringHandling.java
@@ -2,8 +2,8 @@ public class StringHandling
 {
     public static void main(String[] args)
     {
-        String s1="hello";
-        String s2="hello";
+        String s1="Hello";
+        String s2="Hello";
         String s3;
         s3=s1;
         String s4 = new String("Hello");
@@ -31,5 +31,33 @@ public class StringHandling
         {
             System.out.println("s1!=s4");
         }
+        System.out.println(s1);
+        s1.toLowerCase();
+        System.out.println(s1);
+        s1=s1.toUpperCase();
+        System.out.println(s1);
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.charAt(3));
+        System.out.println(s1.compareToIgnoreCase(s2));
+        System.out.println(s1.concat("world"));
+        System.out.println(s1.endsWith("takashi"));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s1.contains("l"));
+        System.out.println(s1.isEmpty());
+        System.out.println("------------------------------------------");
+        String collegeName="Lambton college in Toronto";
+        System.out.println(collegeName.length());
+        System.out.println(collegeName.contains("world"));
+        System.out.println(collegeName.contains("college"));
+        System.out.println(collegeName.indexOf("Toronto"));
+        System.out.println(collegeName.toUpperCase());
+        System.out.println(collegeName.toLowerCase());
+        System.out.println(collegeName.indexOf(10));
+        System.out.println(collegeName.replace("Toronto", "New york"));
+        System.out.println(collegeName.contains("lege"));
+        System.out.println(collegeName.toCharArray());
+        System.out.println(collegeName.concat("265 yorkland blvd., North york"));
+
+
     }
 }
